541c9e6a5244d75e3a5e73a6d59956ec508d7cd6
Sukhmanpreet Singh <ssingh33_be23@thapar.edu>
2026-02-16 12:16:29 +0530

    Add files via upload

---
+++ Topsis Assignment/Topsis-Sukhmanpreet-102317112/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="Topsis-Sukhmanpreet-102317112",
+    version="1.0.0",
+    description="A command line python package to implement TOPSIS",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Sukhmanpreet Singh",
+    packages=["topsis_sukhmanpreet_102317112"],
+    install_requires=['pandas', 'numpy'],
+    entry_points={
+        'console_scripts': [
+            'topsis=topsis_sukhmanpreet_102317112.topsis:main',
+        ],
+    },
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
